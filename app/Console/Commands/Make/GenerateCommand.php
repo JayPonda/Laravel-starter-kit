46674d6a4d3f2e236c9550b6d9bc86eeb332d0ba
Jay Ponda <125702343+JayPonda@users.noreply.github.com>
2026-06-01 11:22:53 +0530

    Remove trailing whitespace in GenerateCommand.php

--- app/Console/Commands/Make/GenerateCommand.php
+++ app/Console/Commands/Make/GenerateCommand.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Console\Commands;
+namespace App\Console\Commands\Make;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\File;
@@ -197,5 +197,3 @@ METHODS;
 METHODS;
     }
 }
-
-PHP;
